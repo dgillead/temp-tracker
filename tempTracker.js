536d3f4846667206b8f1094619330070fcc86571
Darnell Gillead <dlgillead@gmail.com>
2017-08-01 18:31:46 -0500

    added getMean function

--- tempTracker.js
+++ tempTracker.js
@@ -20,14 +20,32 @@ function getMin(temps) {
   return minTemp;
 }
 
+function getMean(temps) {
+  let meanTemp = 0;
+  let sum = 0;
+
+  for (let i = 0; i < temps.length; i++) {
+    sum += temps[i];
+  }
+
+  meanTemp = sum / temps.length;
+
+  return meanTemp.toPrecision(4);
+}
+
 insert(20);
 insert(30);
 insert(40);
 insert(50);
 insert(60);
 insert(100);
+insert(101);
+insert(87);
+insert(55);
 
 let maxTemp = getMax(temps);
 let minTemp = getMin(temps);
+let meanTemp = getMean(temps);
 console.log(maxTemp + ' highest temp');
 console.log(minTemp + ' lowest temp');
+console.log(meanTemp + ' average temp');
